68cea021623640b9fb46c19704206af4c1b17605
abhi543 <ak0787777@gmail.com>
2026-09-07 09:03:47 +0530

    Revert "fix"
    This reverts commit 3ea61544ad3a873a619496913d96b973221cb9ec.

--- app/create-course/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Create a Course | EduGPT",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
